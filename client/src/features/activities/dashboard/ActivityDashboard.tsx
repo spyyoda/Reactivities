8677200f0f748e0082e88647ccafe7b6d5b141d4
Hubert Holda <holdah@hotmail.com>
2025-02-21 10:30:59 -0600

    End of section 6

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,44 @@
+import { Grid2 } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../details/form/ActivityForm";
+
+type Props = {
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  selectedActivity?: Activity | undefined;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  editMode: boolean;
+};
+
+export default function ActivityDashboard({
+  activities,
+  cancelSelectActivity,
+  selectActivity,
+  selectedActivity,
+  openForm,
+  closeForm,
+  editMode
+}: Props) {
+  return (
+    <Grid2 container spacing={3}>
+      <Grid2 size={7}>
+        <ActivityList activities={activities} selectActivity={selectActivity}  />
+      </Grid2>
+      <Grid2 size={5}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            selectedActivity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && (
+          <ActivityForm closeForm={closeForm} activity={selectedActivity} />
+        )}
+      </Grid2>
+    </Grid2>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./app/layout/styles.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import App from "./App.tsx";
+import App from "./app/layout/App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
